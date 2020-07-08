299ba16b87d12e5a0a1d854c82608e7e70fbd133
minhpham8502 <67989550+minhpham8502@users.noreply.github.com>
2020-07-08 17:18:38 +0700

    master

---
+++ app.js
@@ -0,0 +1,21 @@
+function n2(x){
+    i=x*x;
+    console.log(i);
+}
+function chuvi(a,b){
+    y=(a+b)*2;
+    console.log(y);
+}
+function cuuchuong(n){
+    console.log('bai 3: bang cuu chuong cua so ' + n);
+    for(i=0;i<=10;i++){
+        y=n*i;
+        console.log(y)
+    }
+}
+console.log('bai 1');
+n2(2);
+console.log('bai 2');
+chuvi(2,3);
+
+cuuchuong(3);
